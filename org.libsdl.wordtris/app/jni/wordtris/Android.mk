0befa758d1bcfb0782672cb7ff9b7cb9d82e992e
ck <ck@deadlock.cc>
2019-06-15 17:04:31 +0200

    fixed android build and background music

--- org.libsdl.wordtris/app/jni/wordtris/Android.mk
+++ org.libsdl.wordtris/app/jni/wordtris/Android.mk
@@ -7,6 +7,7 @@ LOCAL_MODULE := main
 SDL_PATH := ../SDL
 
 SDL_TTF_PATH := ../ttf
+SDL_MIXER_PATH := ../mixer
 
 LOCAL_C_INCLUDES := \
 					$(LOCAL_PATH)/$(SDL_PATH)/include \
@@ -17,7 +18,7 @@ LOCAL_C_INCLUDES := \
 LOCAL_SRC_FILES :=  \
 					$(wildcard $(LOCAL_PATH)/src/*.cpp) \
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_ttf SDL2_mixer
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -landroid
 
